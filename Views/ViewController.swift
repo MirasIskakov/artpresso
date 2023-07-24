bae406212da771b2f277d5be4c9ab64820d56386
Miras <iskakov>
2023-07-24 20:36:54 +0600

    checkForUnboard

--- Views/ViewController.swift
+++ Views/ViewController.swift
@@ -8,22 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let userDefaults = UserDefaults.standard.bool(forKey: "hasLaunched")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        checkForUnboard()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func checkForUnboard(){
+        if !userDefaults{
+            let vc = storyboard?.instantiateViewController(identifier: "onboard") as! OnboardingViewController
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true)
+        }
     }
-    */
+    
+
 
 }
